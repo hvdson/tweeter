2eb10ef1dac425ec23c7d68cb89b2de36b5cdff6
hvdson <hvdsonmusic@gmail.com>
2018-01-30 21:31:49 +0000

    added text colour change to composer-char-counter when char count was greater than 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,15 +5,36 @@ $(document).ready( () => {
   // let charCounter = $("body .container .new-tweet .counter");
 
   // !!! FAT ARROW FUNCTIONS DO NOT CHANGE THE CONTEXT OF: 'this' inside fntn scope
-  let charCounter = 0;
-  const textArea = $("body .container .new-tweet textarea");
-  textArea.on("keyup", function (event) {
-    // after getting 
-    charCounter = event.target.value.length;
+  // since this method will be called every time keyup event happens
+  // no need to loop
+  // don't need to look through every child node - jQuery functions do the heavy lifting
+
+  $(".new-tweet textarea").on("keyup", function (event) {
+    console.log(event.target.value);
+
+
+    // curr length of textarea
+    // event.target.value.length;
 
     // need to update the value of counter
+    const textAreaLength = event.target.value.length;
 
-    // console.log(charCounter);
-    // console.log(this);
+    if (textAreaLength > 140) {
+      $(".new-tweet .counter").addClass("red-counter");
+    } else {
+      $(".new-tweet .counter").removeClass("red-counter");
+    }
+
+    $(".new-tweet .counter").text(140 - textAreaLength);
+
+    // const currCount = parseInt($(".new-tweet .counter").text(), 10);
+
+    // $(".new-tweet .under-textarea .counter").value = $(".new-tweet .under-textarea .counter").value - event.target.value.length;
+
+    // console.log(event.target.value);
+    // const maxCount = parseInt($(".new-tweet .counter").text(), 10);
+
+
+
   });
 });
