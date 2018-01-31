81d900cab2d33db955acf5190f035b07c8d85ca8
hvdson <hvdsonmusic@gmail.com>
2018-01-31 05:03:30 +0000

    finished task 2: dynamic tweets - need to refactor still

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,21 +5,69 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": {
+//       "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//       "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//     },
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// };
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
 
 // Parameters: tweet object;
 // returns: jQuery object representing a new tweet in the same structure as <article class="tweet">
@@ -59,12 +107,27 @@ function createTweetElement (data) {
   return newTweet;
 }
 
-var $tweet = createTweetElement(tweetData);
+// var $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-// to see what it looks like
-console.log($tweet);
+// // Test / driver code (temporary)
+// // to see what it looks like
+// console.log($tweet);
 
-// to add it to the page so we can make sure it's got all the right elements, classes, etc.
-$('#tweets-container').append($tweet);
+// // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+// $('#tweets-container').append($tweet);
 
+
+function renderTweets(tweets) {
+  
+  
+  
+  // loops through tweets
+  for (let tweet of tweets) {
+    // calls createTweetElement for each tweet
+    let newTweet = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $('#tweets-container').append(newTweet);
+  }
+}
+
+renderTweets(data);
