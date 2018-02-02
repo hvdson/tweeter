3e493a65ca65f2104e38883976d12d47690b297e
hvdson <hvdsonmusic@gmail.com>
2018-02-02 23:42:04 +0000

    changing to like-count branch

--- public/scripts/like-count.js
+++
@@ -1,9 +0,0 @@
-$(document).ready( () => {
-
-  $(this).on("click", function (event) {
-    console.log("inside thing!");
-    // refactoring code
-    const counter = $(".tweet footer i#like");
-    console.log(counter);
-  });
-});
