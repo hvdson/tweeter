d2fc14c39289d3fcb6769e7805e60b09a670a717
hvdson <hvdsonmusic@gmail.com>
2018-02-03 00:53:46 +0000

    added data parameter: like-toggle to like icon which is a boolean that determine whether or not the like button is clicked

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
 
     let flag = $("<i>").addClass("fa fa-flag").attr("id", "flag").attr("aria-hidden", "true");
     let retweet = $("<i>").addClass("fa fa-retweet").attr("id", "retweet").attr("aria-hidden", "true");
-    let like = $("<i>").addClass("fa fa-heart").attr("id", "like").attr("aria-hidden", "true");
+    let like = $("<i>").addClass("fa fa-heart").attr("id", "like").attr("aria-hidden", "true").data("like-toggle", false);
     let likeCount = $("<span>").attr("id", "like-count").text("0");
 
     footerTag.append(timeOfTweet, flag, retweet, likeCount, like);
--- public/scripts/like-count.js
+++ public/scripts/like-count.js
@@ -1,9 +1,21 @@
+// jQuery for getting click on like icon
+// TODO - get click for tweet like button
+// TODO - Update the "like-count" for that specific like
 $(document).ready( () => {
 
   $("#tweets-container").on("click", "#like", function (event) {
-    console.log("inside thing!");
-    // refactoring code
-    const counter = $(".tweet footer i#like");
-    console.log(counter);
+    // const counter = $(this);
+    // "this" is more useful than using event b/c need to get
+    const likeCount = $(this).data("like-toggle");
+    
+    if (!likeCount) {
+      $(this).data("like-toggle", true);
+    } else {
+      $(this).data("like-toggle", false);
+    }
+
+    console.log(likeCount);
+
+    // console.log(counter);
   });
 });
