f006cb00eb9cb9e218fa372a2952f58740536460
hvdson <hvdsonmusic@gmail.com>
2018-02-03 20:13:22 +0000

    fixing bugs with .env file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
+require('dotenv').config();
 "use strict";
 
 // Basic express setup:
-require('dotenv').config();
-
 const PORT          = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
